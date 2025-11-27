8a27bc30ce01d9cec53763fe6e37aa8cd33e38e2
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-27 16:54:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function acciojob(){
 	let functionName=arguments.callee.name;
-	alert(`alert(''functionName''')`);
+	alert(`''${functionName}''`);
 }
 acciojob();
